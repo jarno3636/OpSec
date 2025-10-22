d0525c17b7ced02442a97d6e3b4342d2adca5d0d
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-10-22 10:53:43 -0500

    Delete components/KeyValue.tsx

--- components/KeyValue.tsx
+++
@@ -1,8 +0,0 @@
-export function KeyValue({ k, v }: { k: string; v: React.ReactNode }) {
-  return (
-    <div className="flex justify-between py-1 text-sm">
-      <span className="text-white/60">{k}</span>
-      <span className="font-mono">{v}</span>
-    </div>
-  );
-}
